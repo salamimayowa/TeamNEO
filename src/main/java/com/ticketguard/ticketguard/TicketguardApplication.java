a258b3260224eeb065fcc1355420c3b6f7de26fb
salamiamayowa <salamiabdulbasit123@gmail.com>
2026-03-25 00:08:39 +0100

    add(spring):added the spring file for the first time

---
+++ src/main/java/com/ticketguard/ticketguard/TicketguardApplication.java
@@ -0,0 +1,13 @@
+package com.ticketguard.ticketguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketguard/ticketguard/TicketguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketguard.ticketguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
